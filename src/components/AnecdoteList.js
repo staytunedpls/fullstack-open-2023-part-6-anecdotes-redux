559d169c70a8e08208308a4316cf98733cd39ee1
Sergey Starikov <sergeystarikov96@gmail.com>
2023-10-04 23:25:59 +0100

    Little fix for 6.8

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -9,7 +9,6 @@ const AnecdoteList = () => {
 
   return (
     <div>
-      <h2>Anecdotes</h2>
       {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
