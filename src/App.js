08d7a632c10262460fe439f0218f98f0f29f0610
Sergey Starikov <sergeystarikov96@gmail.com>
2023-10-06 23:48:08 +0100

    Done 6.14

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { useDispatch } from "react-redux";
+import { setAll } from "./reducers/anecdoteReducer";
+import anecdoteService from "./services/anecdotes";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => dispatch(setAll(anecdotes)));
+  });
   return (
     <div>
       <h2>Anecdotes</h2>
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
 const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
@@ -21,7 +12,7 @@ const asObject = (anecdote) => {
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState: anecdotesAtStart.map(asObject),
+  initialState: [],
   reducers: {
     vote(state, action) {
       return state.map((anecdote) =>
@@ -33,8 +24,11 @@ const anecdoteSlice = createSlice({
     create(state, action) {
       return state.concat(asObject(action.payload));
     },
+    setAll(state, action) {
+      return action.payload
+    }
   },
 });
 
-export const { vote, create } = anecdoteSlice.actions;
+export const { vote, create, setAll } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
