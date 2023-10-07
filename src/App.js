77a4a845d7420f87821a4000a0daeab105f8d0c1
Sergey Starikov <sergeystarikov96@gmail.com>
2023-10-07 02:04:00 +0100

    Done 6.16

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
 import { useDispatch } from "react-redux";
-import { setAll } from "./reducers/anecdoteReducer";
-import anecdoteService from "./services/anecdotes";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    anecdoteService.getAll().then((anecdotes) => dispatch(setAll(anecdotes)));
+    dispatch(initializeAnecdotes());
   }, [dispatch]);
   return (
     <div>
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
@@ -19,6 +20,13 @@ const anecdoteSlice = createSlice({
     },
   },
 });
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAll(anecdotes));
+  };
+};
 
 export const { vote, create, setAll } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
